7fd7bed81de628ba3b2cfb714b2fdca7d94af891
Quentin Santos <qsantos@qsantos.fr>
2024-08-03 11:49:28 +0200

    Fix handling of already present files

--- searchedict/edict/get.py
+++ searchedict/edict/get.py
@@ -108,7 +108,7 @@ def atomic_extract(in_filename, out_filename, progress_callback=None, progress_s
 def fetch_and_extract(url, filename, progress_callback=None, progress_step=2**-6):
     # do nothing is file already exists
     if os.path.isfile(filename):
-        return None
+        return True
 
     # fetch compressed file
     gz_filename = filename + '.gz'
