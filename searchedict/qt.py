3b0ccd22585ac867d9224b238a9fc7fd865878c0
Quentin Santos <qsantos@qsantos.fr>
2024-08-22 17:57:29 +0200

    Ignore mypy warnings about conditional imports

--- searchedict/qt.py
+++ searchedict/qt.py
@@ -1,9 +1,9 @@
 try:
-    from PyQt6 import QtCore, QtGui
+    from PyQt6 import QtCore, QtGui # type: ignore[import-not-found]
 except ImportError:
     from PyQt5 import QtCore, QtGui
     from . import formbrowser_qt5 as formbrowser
     from . import formsettings_qt5 as formsettings
 else:
-    from . import formbrowser_qt6 as formbrowser
-    from . import formsettings_qt6 as formsettings
+    from . import formbrowser_qt6 as formbrowser  # type: ignore[no-redef]
+    from . import formsettings_qt6 as formsettings  # type: ignore[no-redef]
