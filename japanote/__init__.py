8c45696365fd0a7461db29a65865a8431fde407c
Quentin Santos <qsantos@qsantos.fr>
2024-09-04 18:21:46 +0200

    Remove menu entry

--- japanote/__init__.py
+++ japanote/__init__.py
@@ -1,24 +1,4 @@
-from anki.hooks import addHook
-from aqt import mw
-from aqt.browser import Browser
-from aqt.qt import QAction
-
 from .quickadd import QuickAddModule
-from .searchwindow import SearchWindow
 
 
-def add_menu_entries_to_browser(self: Browser) -> None:
-    menu = self.form.menuEdit
-    menu.addSeparator()
-
-    action = QAction('Browse EDICT...', mw)
-    action.triggered.connect(lambda: SearchWindow.open())
-    menu.addAction(action)
-
-
-def main() -> None:
-    addHook('browser_menus_did_init', add_menu_entries_to_browser)
-    QuickAddModule().display()
-
-
-main()
+QuickAddModule().display()
