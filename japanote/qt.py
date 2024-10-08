4cbf09c0bd0b9d1d9c370ed83874207e3423ee1a
Quentin Santos <qsantos@qsantos.fr>
2024-10-08 22:42:48 +0200

    Fix mypy not using PyQt stubs at all

--- japanote/qt.py
+++ japanote/qt.py
@@ -1,9 +1,9 @@
 try:
-    from PyQt6 import QtCore, QtGui # type: ignore[import-not-found]
-except ImportError:
     from PyQt5 import QtCore, QtGui
-    from . import searchwindow_qt5 as searchwindow
-    from . import settingswindow_qt5 as settingswindow
+except ImportError:
+    from PyQt6 import QtCore, QtGui  # type: ignore[import-not-found, no-redef]
+    from . import searchwindow_qt6 as searchwindow
+    from . import settingswindow_qt6 as settingswindow
 else:
-    from . import searchwindow_qt6 as searchwindow  # type: ignore[no-redef]
-    from . import settingswindow_qt6 as settingswindow  # type: ignore[no-redef]
+    from . import searchwindow_qt5 as searchwindow  # type: ignore[no-redef]
+    from . import settingswindow_qt5 as settingswindow  # type: ignore[no-redef]
